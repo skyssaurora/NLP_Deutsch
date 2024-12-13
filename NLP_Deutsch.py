4700300f0f31f34893871c5ac7065762bcc1d76c
skyssaurora <skyssaurora@gmail.com>
2024-12-13 03:09:01 +0000

    edit foto

--- NLP_Deutsch.py
+++ NLP_Deutsch.py
@@ -334,7 +334,7 @@ page_bg_img = f"""
 [data-testid="stAppViewContainer"] {{
 background-image: url("data:image/png;base64,{get_img_as_base64("./Images/BARU.JPG")}");
 background-size: cover;
-background-position: top center; 
+background-position: bottom center; 
 background-repeat: no-repeat;
 background-attachment: fixed;
 }}
