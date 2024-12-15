f1f6bc44eacf049ad60198f5790dee3ebe5dfb37
skyssaurora <skyssaurora@gmail.com>
2024-12-15 07:25:01 +0000

    edit

--- NLP_Deutsch.py
+++ NLP_Deutsch.py
@@ -139,7 +139,7 @@ def get_img_as_base64(file):
 
 with st.sidebar:
     
-    st.header('(§) Syntax Spion')
+    st.header('§ Syntax Spion')
     st.html("""
         <p style="color: #ede1c7;
             font-family: Royal Avenue">
