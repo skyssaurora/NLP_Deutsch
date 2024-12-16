9073107c8db12ae7038c7ab9e4e61b6a7569237c
skyssaurora <skyssaurora@gmail.com>
2024-12-16 08:11:13 +0000

    edit nama

--- NLP_Deutsch.py
+++ NLP_Deutsch.py
@@ -143,7 +143,7 @@ with st.sidebar:
     st.html("""
         <p style="color: #ede1c7;
             font-family: Royal Avenue">
-            <a style="font-weight: 600;">Created by:</a> AB, Namu, Gwen, Augustine, Yuphoria, Fore
+            <a style="font-weight: 600;">Created by:</a> AB, Augustine, Fore, Gwen, Namu, Yuphoria
         </p>
         <p style="color: #ede1c7; 
             margin: 0 0 .5rem; 
